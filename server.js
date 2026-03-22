0a84827b07effdcdee618244ec038f333fe2477f
Santhosh Devale <santhosh.rao@egovernments.org>
2026-03-22 09:21:28 +0530

    updated corn code

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://your-frontend.vercel.app", // 🔁 replace with your actual frontend URL
+      FRONTEND_URL, // 🔁 replace with your actual frontend URL
     ],
   })
 );
